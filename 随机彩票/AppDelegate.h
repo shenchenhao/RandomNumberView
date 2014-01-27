61f00e97d0c647c5990690adde02dc0b9145191c
shenchenhao <shenchenhao131@163.com>
2014-01-27 14:00:54 +0800

    Initial Commit

---
+++ 随机彩票/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  随机彩票
+//
+//  Created by 沈 晨豪 on 14-1-27.
+//  Copyright (c) 2014年 sch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 随机彩票/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  随机彩票
+//
+//  Created by 沈 晨豪 on 14-1-27.
+//  Copyright (c) 2014年 sch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
